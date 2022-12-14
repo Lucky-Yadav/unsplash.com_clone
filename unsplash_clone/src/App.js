83317c50e80df86d2f97d59d952474298628a324
Lucky yadav <luckyyadav8627@gmail.com>
2022-12-14 20:06:50 +0530

    updae package and app

--- unsplash_clone/src/App.js
+++ unsplash_clone/src/App.js
@@ -4,10 +4,10 @@ import { Navbar } from "./components/Navbar/Navbar"
 import { Route, Routes } from 'react-router';
 import { SignUp } from './components/Auth/Signup';
 import { LogIn } from './components/Auth/Login';
-import { Blog } from "./Pages/Blog";
-import { Inputform } from './components/Advertise/Inputform';
-import {Advertise} from "./Pages/Advertise";
-import { SuccessForm } from "./components/Advertise/SuccessForm";
+// import { Blog } from "./Pages/Blog";
+// import { Inputform } from './components/Advertise/Inputform';
+// import {Advertise} from "./Pages/Advertise";
+// import { SuccessForm } from "./components/Advertise/SuccessForm";
 import { Error } from "./components/Navbar/Error"
 import Home from './home';
 
@@ -20,14 +20,14 @@ function App() {
         <Route path="/" element={<Navbar ><Home query = {"office"} /></Navbar>}></Route>
         {/* <Route path="/" element={<Home/> }></Route> */}
         {/* <Route path="/error" element={<Error />}></Route> */}
-        <Route path="/advertise" element={<Advertise />}></Route>
+        {/* <Route path="/advertise" element={<Advertise />}></Route> */}
       </Routes>
       <Routes>
-        <Route path="/Inputform" element={<Inputform />}></Route>
-        <Route path="/SuccessForm" element={<SuccessForm />}></Route>
+        {/* <Route path="/Inputform" element={<Inputform />}></Route> */}
+        {/* <Route path="/SuccessForm" element={<SuccessForm />}></Route> */}
         <Route path="/login" element={<LogIn />}></Route>
         <Route path="/signup" element={<SignUp />}></Route>
-        <Route path="/blog" element={<Blog />}></Route>
+        {/* <Route path="/blog" element={<Blog />}></Route> */}
       </Routes>
       {/* <h1>hello</h1> */}
     </div>
