76ab44b3c4a47792385393126c3a6201dbfb16ae
Lucky yadav <luckyyadav8627@gmail.com>
2023-03-24 19:55:11 +0530

    Update packages and index file

--- unsplash_clone/src/index.js
+++ unsplash_clone/src/index.js
@@ -4,35 +4,23 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "./Components/Redux/store";
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//   <BrowserRouter>
-//     <CssBaseline>
-//       <App />
-//     </CssBaseline>
-//   </BrowserRouter>
-// );
+// import { Provider } from "react-redux";
+// import { store } from "./Components/Redux/store";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
 // import { ScrollerMotion } from 'scroller-motion';
 
-// import {render} from
-// const container = (document.getElementById('root'));
-// root.render(
-//   <>
-//   <Provider store={ store }>
-//   <BrowserRouter>
-//     <App />
-//   </BrowserRouter>
-//   </Provider>
-//   </>
-// );
 
-ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById("root")
-);
+
+// ReactDOM.render(
+//   // <Provider store={store}>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>,
+//   // </Provider>,
+//   document.getElementById("root")
+// );
