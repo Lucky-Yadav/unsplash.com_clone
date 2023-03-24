a7643a95e587a443dfbf29caf502a199d98847ec
Lucky yadav <luckyyadav8627@gmail.com>
2023-03-24 20:31:30 +0530

    Update store.js

--- unsplash_clone/src/components/Redux/store.js
+++ unsplash_clone/src/components/Redux/store.js
@@ -1,4 +1,12 @@
-import { legacy_createStore as createStore } from "redux";
+// import { legacy_createStore as createStore } from "redux";
 import { AuthReducer } from "./reducer";
+
+// export const store = createStore(AuthReducer);
+
+import {  createStore } from "redux";
+
+// const rootReducer = combineReducers({
+//     auth: authReducer
+// })
 
 export const store = createStore(AuthReducer);
